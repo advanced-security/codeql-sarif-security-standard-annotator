fa4216045145e64f9f482a8282777c49da48281d
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2023-04-28 13:50:13 +0100

    Refactor and improve input options (#8)

--- README.md
+++ README.md
@@ -1,105 +1,29 @@
-<p align="center">
-  <a href="https://github.com/actions/typescript-action/actions"><img alt="typescript-action status" src="https://github.com/actions/typescript-action/workflows/build-test/badge.svg"></a>
-</p>
+# codeql-sarif-security-standard-annotator
 
-# Create a JavaScript Action using TypeScript
+Compare a CodeQL SARIF results file to a security standard CWE list and annotate the SARIF rules with a tag to highlight results applicable to the security standard
 
-Use this template to bootstrap the creation of a TypeScript action.:rocket:
+## Usage in GitHub Actions
 
-This template includes compilation support, tests, a validation workflow, publishing, and versioning guidance.  
-
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
-
-## Create an action from this template
-
-Click the `Use this Template` and provide the new repo details for your action
-
-## Code in Main
-
-> First, you'll need to have a reasonably modern version of `node` handy. This won't work with versions older than 9, for instance.
-
-Install the dependencies  
-```bash
-$ npm install
+```
+- name: Annotate CodeQL SARIF with OWASP Top 10 2021 tag
+  uses: ctcampbell/codeql-sarif-security-standard-annotator@v1
 ```
 
-Build the typescript and package it for distribution
-```bash
-$ npm run build && npm run package
 ```
-
-Run the tests :heavy_check_mark:  
-```bash
-$ npm test
-
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
-
-...
+inputs:
+  sarifFile:
+    required: true
+    description: 'The CodeQL SARIF result file'
+  cweFile:
+    required: false
+    description: 'The CWE list XML file'
+    default: '${{ github.action_path }}/security-standards/owasp-top10-2021.xml'
+  securityStandardTag:
+    required: false
+    description: 'The security standard tag to add to the SARIF file'
+    default: 'owasp-top10-2021'
+  outputFile:
+    required: false
+    description: 'The output SARIF file path, defaults to the input SARIF file path'
+    default: '${{ inputs.sarifFile }}'
 ```
-
-## Change action.yml
-
-The action.yml defines the inputs and output for your action.
-
-Update the action.yml with your name, description, inputs and outputs for your action.
-
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
-
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
-```javascript
-import * as core from '@actions/core';
-...
-
-async function run() {
-  try { 
-      ...
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
-```
-
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
-
-## Publish to a distribution branch
-
-Actions are run from GitHub repos so we will checkin the packed dist folder. 
-
-Then run [ncc](https://github.com/zeit/ncc) and push the results:
-```bash
-$ npm run package
-$ git add dist
-$ git commit -a -m "prod dependencies"
-$ git push origin releases/v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket: 
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Validate
-
-You can now validate the action by referencing `./` in a workflow in your repo (see [test.yml](.github/workflows/test.yml))
-
-```yaml
-uses: ./
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/typescript-action/actions) for runs of this action! :rocket:
-
-## Usage:
-
-After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,68 @@
 import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-import * as fs from 'fs'
+import {hideBin} from 'yargs/helpers'
+import {readFileSync, writeFileSync} from 'fs'
 import * as core from '@actions/core'
-import { parseXml, XmlElement } from '@rgrove/parse-xml'
-import { env } from 'process'
+import {DOMParser} from '@xmldom/xmldom'
+import * as xpath from 'xpath'
+import type {Log} from 'sarif'
 
-function isXmlElement(xmlThing: any): xmlThing is XmlElement {
-  return xmlThing instanceof XmlElement;
-}
+// Global variables
+let sarifResults: Log
+let cweXml: Document
+const codeQlCweTagPrefix = 'external/cwe/cwe-'
+const xmlNs = {cwe: 'http://cwe.mitre.org/cwe-6'}
+const cweIdXpath = '/cwe:Weakness_Catalog/cwe:Weaknesses/cwe:Weakness/@ID'
 
-// parse inputs or arguments from command line
-var sarifFile = core.getInput('sarifFile');
-var cweFile = core.getInput('cweFile');
-
-if (env.CI !== 'true') {
-  const argv = yargs(hideBin(process.argv)).options({
+// Parse arguments
+const argv = yargs(hideBin(process.argv))
+  .options({
     sarifFile: {type: 'string', demandOption: true},
-    cweFile: {type: 'string', demandOption: true}
-  }).parseSync();
-  sarifFile = argv.sarifFile;
-  cweFile = argv.cweFile;
-}
+    cweFile: {type: 'string', demandOption: true},
+    securityStandardTag: {type: 'string', demandOption: true},
+    outputFile: {type: 'string', demandOption: true}
+  })
+  .parseSync()
 
-// load SARIF file
+// Load SARIF file
 try {
-  var sarifResults = JSON.parse(fs.readFileSync(sarifFile, 'utf8'));
+  sarifResults = JSON.parse(readFileSync(argv.sarifFile, 'utf8'))
 } catch (err) {
-  core.setFailed(`Unable to load SARIF file: ${err}`);
-  process.exit(1);
+  core.setFailed(`Unable to load SARIF file: ${err}`)
+  process.exit(1)
 }
 
-// load security standard CWE list
+// Load security standard CWE XML file
 try {
-  var cweList = parseXml(fs.readFileSync(cweFile, 'utf8'));
+  cweXml = new DOMParser().parseFromString(readFileSync(argv.cweFile, 'utf8'))
 } catch (err) {
-  core.setFailed(`Unable to load CWE list: ${err}`);
-  process.exit(1);
+  core.setFailed(`Unable to load CWE file: ${err}`)
+  process.exit(1)
 }
+const select = xpath.useNamespaces(xmlNs)
+const cweIdAttributes = select(cweIdXpath, cweXml).filter((x): x is Attr => Object.getPrototypeOf(x).constructor.name === 'Attr')
+const cweIdArray = cweIdAttributes.map(attribute => attribute.value)
 
-var cweIDList: number[] = []
-
-cweList.children.filter(isXmlElement).forEach(child => {
-  if (child.name === 'Weakness_Catalog') {
-    child.children.filter(isXmlElement).forEach(child => {
-      if (child.name === 'Weaknesses') {
-        child.children.filter(isXmlElement).forEach(weakness => {
-          cweIDList.push(parseInt(weakness.attributes.ID));
-        })
-      }
-    })
-  }
-})
-
-cweIDList.sort()
-
-// Annotate SARIF file
-sarifResults.runs.forEach((run: any) => {
-  run.tool.extensions?.forEach((extension: any) => {
-    extension.rules?.forEach((rule: any) => {
-      rule.properties?.tags?.forEach((tag: string) => {
-        if (tag.startsWith('external/cwe/cwe-')) {
-          const cweId = tag.split('-').pop();
-          if (cweId !== undefined && cweIDList.includes(parseInt(cweId)) && !rule.properties.tags.includes('owasp-2021')) {
-            rule.properties.tags.push('owasp-2021');
+// Add tag to SARIF file
+for (const run of sarifResults.runs) {
+  for (const extension of run.tool.extensions || []) {
+    for (const rule of extension.rules || []) {
+      for (const tag of rule.properties?.tags || []) {
+        if (tag.startsWith(codeQlCweTagPrefix)) {
+          const cweId = tag.replace(codeQlCweTagPrefix, '')
+          if (cweIdArray.includes(cweId)) {
+            rule.properties?.tags?.push(argv.securityStandardTag)
+            break
           }
         }
-      })
-    })
-  })
-})
+      }
+    }
+  }
+}
 
-// Output report
+// Output SARIF file with tag added
 try {
-  fs.writeFileSync(sarifFile, JSON.stringify(sarifResults))
+  writeFileSync(argv.outputFile, JSON.stringify(sarifResults))
 } catch (err) {
-  core.setFailed(`Unable to write SARIF file: ${err}`);
-  process.exit(1);
+  core.setFailed(`Unable to write SARIF file: ${err}`)
+  process.exit(1)
 }

ab23ffe6dd14443727754e8def56020afe60b17a
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2023-04-28 19:05:32 +0000

    Fix file paths

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import {env} from 'process'
+import {resolve} from 'path'
 import {readFileSync, writeFileSync} from 'fs'
 import * as core from '@actions/core'
 import {DOMParser} from '@xmldom/xmldom'
@@ -9,16 +9,16 @@ import type {Log} from 'sarif'
 // Global variables
 let sarifResults: Log
 let cweXml: Document
-const defaultCweFilePath = `${env.GITHUB_ACTION_PATH}/security-standards/owasp-top10-2021.xml`
+const defaultCweFilePath = `${__dirname}/security-standards/owasp-top10-2021.xml`
 const codeQlCweTagPrefix = 'external/cwe/cwe-'
 const xmlNs = {cwe: 'http://cwe.mitre.org/cwe-6'}
 const cweIdXpath = '/cwe:Weakness_Catalog/cwe:Weaknesses/cwe:Weakness/@ID'
 
 // Parse Actions inputs
-const sarifFilePath = core.getInput('sarifFile')
-const cweFilePath = core.getInput('cweFile') || defaultCweFilePath
+const sarifFilePath = resolve(core.getInput('sarifFile'))
+const cweFilePath = resolve(core.getInput('cweFile') || defaultCweFilePath)
 const securityStandardTag = core.getInput('securityStandardTag')
-const outputFilePath = core.getInput('outputFile') || sarifFilePath
+const outputFilePath = resolve(core.getInput('outputFile') || sarifFilePath)
 
 console.log(`Using ${sarifFilePath} for SARIF file`)
 console.log(`Using ${cweFilePath} for CWE file`)
